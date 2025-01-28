a6158fef81220663161d38a602966726576e0a0b
summer3018 <4121.iskms@gmail.com>
2025-01-28 16:50:38 +0900

    Auto commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// パララックス効果を適用
+// document.addEventListener('scroll', () => {
+//     const image = document.querySelector('.parallax-image');
+//     const scrollPosition = window.scrollY;
+//     const parallaxSpeed = 0.5; // スクロールの速さを調整
+
+//     if (image) {
+//         image.style.transform = `translateY(${scrollPosition * parallaxSpeed}px)`;
+//     }
+// });
