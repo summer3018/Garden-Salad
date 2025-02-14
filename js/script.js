79f504833387a27605dc2acea02037f658054324
summer3018 <4121.iskms@gmail.com>
2025-02-14 15:41:26 +0900

    Auto commit

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ document.addEventListener('scroll', () => {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const pageTopBtn = document.querySelector(".page-top__btn");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 200) {
+            pageTopBtn.style.opacity = "1";  // 表示
+            pageTopBtn.style.pointerEvents = "auto";
+        } else {
+            pageTopBtn.style.opacity = "0";  // 非表示
+            pageTopBtn.style.pointerEvents = "none";
+        }
+    });
+
+    pageTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" }); // スムーズにトップへスクロール
+    });
+});
