8f51000eb015cf914d66868c91b2e5274aca3054
summer3018 <4121.iskms@gmail.com>
2025-03-06 14:32:30 +0900

    Auto commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// パララックス
-document.addEventListener('scroll', () => {
-    const image = document.querySelector('.parallax-image');
-    const scrollPosition = window.scrollY;
-    const parallaxSpeed = 0.01; // スクロールの速さを調整
-
-    if (image) {
-        image.style.transform = `translateY(${scrollPosition * parallaxSpeed}px)`;
-    }
-});
-
 // topへ戻るボタン
 document.addEventListener("DOMContentLoaded", function () {
     const pageTopBtn = document.querySelector(".page-top__btn");
